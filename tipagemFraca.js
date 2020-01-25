a7a13742e91bd7d62262d4c30bbab454871c75ea
italo manoel <manoel.italo89@gmail.com>
2020-01-24 21:15:44 -0300

    vaii

---
+++ tipagemFraca.js
@@ -0,0 +1,7 @@
+let qualquer = 'Legal';
+console.log(qualquer);
+console.log(typeof qualquer); //typeof para saber qual tipo da variavel
+
+qualquer = 3.1516;
+console.log(qualquer);
+console.log(typeof qualquer);
--- variaveisConstantes.js
+++ variaveisConstantes.js
@@ -1,1 +1,15 @@
 var a = 3;
+let b = 4; //recomendado usar let mais que o var
+
+var a = 30;
+b = 40;
+console.log(a,b);
+
+a = a + 300;
+b = 400;
+console.log(a,b);
+
+const c = 5;
+// c = 50; não irá mudar por ser const mesmo colocando c
+console.log(c)
+
