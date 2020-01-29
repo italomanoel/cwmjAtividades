bdfedeb54b59d1d18ae3059fe6d707a56f3695af
italo manoel <manoel.italo89@gmail.com>
2020-01-29 15:09:55 -0300

    aula de numeros

--- numeros.js
+++ numeros.js
@@ -11,6 +11,7 @@ const total = avaliacao1 * peso1 + avaliacao2 * peso2;
 const media = total / (peso1 + peso2);
 
 console.log(media.toFixed(2));
-console.log(media.toString());
+console.log(media.toString(2));
 console.log(typeof media);
+console.log(typeof Number);
 
