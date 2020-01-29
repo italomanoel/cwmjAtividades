f781af31ad42f1f3c15f803e0d91719ecf23f6b1
italo manoel <manoel.italo89@gmail.com>
2020-01-29 19:45:50 -0300

    parou em Math

--- numeros.js
+++ numeros.js
@@ -11,7 +11,7 @@ const total = avaliacao1 * peso1 + avaliacao2 * peso2;
 const media = total / (peso1 + peso2);
 
 console.log(media.toFixed(2));
-console.log(media.toString(2));
+console.log(media.toString()); //colocando o (2) para colocar o valor da média em binário
 console.log(typeof media);
 console.log(typeof Number);
 
