f2a7f9c358ab367a82503dde76802b087d252865
Italo Manoel <manoel.italo89@gmail.com>
2020-01-30 19:14:05 -0300

    feito math

--- numeros.js
+++ numeros.js
@@ -13,5 +13,5 @@ const media = total / (peso1 + peso2);
 console.log(media.toFixed(2));
 console.log(media.toString()); //colocando o (2) para colocar o valor da média em binário
 console.log(typeof media);
-console.log(typeof Number);
+console.log(typeof Number); 
 
--- numerosAlgunsCuidados.js
+++ numerosAlgunsCuidados.js
@@ -2,4 +2,4 @@ console.log(7 / 0); //no javascript ele vai rodar para infinito "Infinity".
 console.log("10"/ 2);
 console.log("Show!" * 2);
 console.log(0.1 + 0.7);
-console.log(10).toFixed
+console.log(10).toFixed(2);
