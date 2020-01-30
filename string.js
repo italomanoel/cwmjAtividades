f4f7b29e77591ad6d7284e39e6407d53e1f13f55
Italo Manoel <manoel.italo89@gmail.com>
2020-01-30 19:35:59 -0300

    tipos string

---
+++ string.js
@@ -0,0 +1,11 @@
+const escola = "CodeNight"
+
+console.log(escola.charAt(1)); //'charAt' para pegar o caractere naquela posição
+console.log(escola.charAt(2));
+console.log(escola.charCodeAt(2)); //saber o código relacionado a tabela unicode (unicode.com.br)
+console.log(escola.indexOf('o')); //retorna posição que está no indice;
+
+
+console.log(escola.substring(2)); //vai de um íncice até o colocado ou apartir.
+console.log(escola.substring(0, 3)); //pega no indice informado até o final informado.
+
