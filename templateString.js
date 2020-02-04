97beb9ae7809ca7009fe539e0aa4667a61ee65c4
italo manoel <manoel.italo89@gmail.com>
2020-02-04 20:07:00 -0300

    em templates

--- templateString.js
+++ templateString.js
@@ -1,8 +1,11 @@
 const nome = 'Kuro';
-const vamosConcatenar = 'Concatena isso com ' + nome + '!';
+const vamosConcatenar = 'Resolve isso com  ' + nome + '!';
 const template = `
-Olá
-${nome}!`         //interpolar: convertendo a variável no próprio texto. Abre com o símbolo da crase ` e coloca $
+    Olá  
+    ${nome}!`         //interpolar: convertendo a variável no próprio texto. Abre com o símbolo da crase ` e coloca $
 console.log(vamosConcatenar, template);
-
 console.log(`1 + 1 = ${1 + 1}`); //expressoes, caso não tenha o $ ele irá concatenar não somar.
+
+const up = texto => texto.toUpperCase()
+console.log(`1 + 1 = ${1 + 1}`);
+
