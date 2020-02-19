44f5df21d18d456f608699ceb38fe7ad0abcb31a
italo manoel <manoel.italo89@gmail.com>
2020-02-19 20:37:39 -0300

    no if else if conm

---
+++ if2.js
@@ -0,0 +1,14 @@
+function teste1(numero){
+    if(numero > 7) //não é obrigatorio uso de chaves, ele irá pegar a linha seguinte apenas. 
+        console.log(numero) 
+
+    console.log('Final')
+};
+teste1(5);
+function teste2(numero){
+    if(numero > 7); { //not
+        console.log
+    }
+}
+
+teste2(6)
